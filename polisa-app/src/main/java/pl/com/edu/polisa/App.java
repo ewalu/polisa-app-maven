ac3215576a73a6058fe3861287a2d5d9cfb9655a
ewalu <ewa_lugin@tlen.pl>
2017-12-03 12:16:59 +0100

    maven

---
+++ polisa-app/src/main/java/pl/com/edu/polisa/App.java
@@ -0,0 +1,13 @@
+package pl.com.edu.polisa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
